78fd5fc42937bafa50f328690b522e8b7a74a971
Anton Petkov <anton.petkoff@gmail.com>
2017-06-29 17:32:20 +0300

    Videos list links

--- app/views/videos/all.php
+++ app/views/videos/all.php
@@ -1,4 +1,6 @@
 <?php foreach($params['videos'] as $video): ?>
-	<h2><?php echo $video['name']; ?></h2>
+  <a href="index.php?q=videos/view/<?php echo $video['id']; ?>">
+    <h2><?php echo $video['name']; ?></h2>
+  </a>
 	<p><?php echo $video['url']; ?></p>
 <?php endforeach; ?>
