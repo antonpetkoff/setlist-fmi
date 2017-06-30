1cdfdab68988a0daf5cc29ee3747bad8e001474d
Anton Petkov <anton.petkoff@gmail.com>
2017-06-30 23:23:25 +0300

    Simple style

--- app/views/videos/all.php
+++ app/views/videos/all.php
@@ -1,6 +1,9 @@
-<?php foreach($params['videos'] as $video): ?>
-  <a href="index.php?q=videos/view/<?php echo $video['id']; ?>">
-    <h2><?php echo $video['name']; ?></h2>
-  </a>
-	<p><?php echo $video['url']; ?></p>
-<?php endforeach; ?>
+<ul>
+  <?php foreach($params['videos'] as $video): ?>
+    <li>
+      <a href="index.php?q=videos/view/<?php echo $video['id']; ?>">
+        <h2>Лекция: <?php echo $video['name']; ?></h2>
+      </a>
+    </li>
+  <?php endforeach; ?>
+</ul>
--- app/views/videos/view.php
+++ app/views/videos/view.php
@@ -1,3 +1,2 @@
-<h2><?php echo $params['video']['name']; ?></h2>
-<h2><?php echo $params['video']['url']; ?></h2>
+<h2>Вие гледате лекция: <?php echo $params['video']['name']; ?></h2>
 <video controls src="<?php echo $params['video']['url']; ?>" width="640" height="480"></video>
