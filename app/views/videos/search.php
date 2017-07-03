b4ffe07066840c0dd9e928ca73b2c170d4a3b300
Anton Petkov <anton.petkoff@gmail.com>
2017-07-03 18:54:13 +0300

    Search style

--- app/views/videos/search.php
+++ app/views/videos/search.php
@@ -1,6 +1,6 @@
 <div class="search-container">
   <form class="search-field" method="POST" action="index.php?q=videos/all">
-    <input name="filter" type="text" placeholder="Търси" />
+    <input name="filter" type="text" placeholder="Търси лекция" />
     <input name="submit" type="submit" value="Търси" />
   </form>
 </div>
