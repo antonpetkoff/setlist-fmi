67d8e5acab8c5b4256ff13d28a4fa7ad3a108ec3
Anton Petkov <anton.petkoff@gmail.com>
2017-07-03 09:06:34 +0300

    Add courses

---
+++ app/views/courses/add.php
@@ -0,0 +1,7 @@
+<h2>Add course</h2>
+
+<form method="POST" enctype="multipart/form-data">
+  <input type="name" />
+  <input type="file" name="file" />
+  <input type="submit" name="submit" value="Upload" />
+</form>
---
+++ app/views/courses/view.php
@@ -0,0 +1,3 @@
+<h2><?php echo $params['course']['title']; ?></h2>
+<h3><?php echo $params['course']['teacher']; ?></h3>
+<p><?php echo $params['course']['description']; ?></p>
